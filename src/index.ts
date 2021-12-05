2fed972f5edc3a525fb6f224e28351dbcce0eaa7
TemidoRocha <manueltemidorocha@gmail.com>
2021-12-05 19:41:13 +0000

    DB connectin with mongoDB drivers instead of mongoose

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,14 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { MongoClient } from 'mongodb';
 
 // express doesn't handle async erros by itself
 import 'express-async-errors';
 
 dotenv.config();
 
-import connectDB from './database/connection';
 import errorHandler from './errors/handler';
-import { createConnection } from 'mongoose';
 
 /**
  * App Variables
@@ -25,6 +24,8 @@ if (!process.env.PORT) {
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
+
+let URI: string = process.env.MONGO_DB_URI || '';
 
 const app = express();
 
@@ -45,15 +46,20 @@ app.use(errorHandler);
  */
 async function initiate(): Promise<void> {
   try {
-    let connection: void | Error = await connectDB();
-    if (connection instanceof Error) {
-      throw new Error(connection.message);
-    }
+    const client: MongoClient = new MongoClient(URI);
+    console.log(`MongoDb connected to:\n ${URI}`);
+
+    await client.connect();
+    // await MoviesDAO.injectDB(client);
+    // await UsersDAO.injectDB(client);
+    // await CommentsDAO.injectDB(client);
     app.listen(PORT, () => {
       console.log(`Listen on port ${PORT}`);
     });
-  } catch (error) {
-    console.log(`initiate error: ${error}`);
+  } catch (error: any) {
+    console.log('### initiate error:');
+    console.error(error.stack);
+    process.exit(1);
   }
 }
 
